c46402b65cd889ff304a2a7ee21411115dd491b2
Jake McKenzie <jake314@uw.edu>
2025-03-24 23:22:57 -0600

    added types to textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -9,18 +10,22 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(
+            self, 
+            text: str, 
+            text_type: TextType, 
+            url:Optional[str] = None
+        ) -> None:
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, TextNode):
             return NotImplemented
         return (self.text == other.text and
                 self.text_type == other.text_type and
                 self.url == other.url)
 
-    def __repr__(self):
-        # Using .value on the enum for a string representation.
+    def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
