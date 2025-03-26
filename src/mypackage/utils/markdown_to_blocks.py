77d1c820d2cfab6416deff34f45df83292407e54
Jake McKenzie <jake314@uw.edu>
2025-03-25 23:34:25 -0600

    fixed imports

--- src/mypackage/utils/markdown_to_blocks.py
+++ src/mypackage/utils/markdown_to_blocks.py
@@ -1,3 +1,6 @@
-def markdown_to_blocks(markdown: str) -> list:
+from typing import List
+
+
+def markdown_to_blocks(markdown: str) -> List[str]:
     blocks = markdown.split("\n\n")
     return [block.strip() for block in blocks if block.strip()]
