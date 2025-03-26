bb4437211895d3bbdd6ab25352f423a98eab1fa1
Jake McKenzie <jake314@uw.edu>
2025-03-25 18:28:49 -0600

    lowered coupling in split_nodes_image_and_link

--- src/mypackage/utils/split_nodes_image_and_link.py
+++ src/mypackage/utils/split_nodes_image_and_link.py
@@ -2,44 +2,68 @@ from typing import List, Callable
 from src.mypackage.text_node import TextNode, TextType
 from src.mypackage.utils.extract_links import extract_markdown_images, extract_markdown_links
 
-def _split_node(
-        node: TextNode,       
-        extract_func: Callable[[str], List[tuple]],
-        format_func: Callable[[str, str], str],
-        new_type: TextType
-    ) -> List[TextNode]:
-    
-    if node.text_type != TextType.NORMAL or not node.text:
-        return [node]
-    
+def should_split(
+    node: TextNode, 
+    extract_func: Callable[[str], List[tuple]]
+) -> bool:
+    return (
+        node.text_type == TextType.NORMAL and 
+        bool(node.text) and 
+        bool(extract_func(node.text
+        )
+    )
+)
+
+def split_node_once(
+    node: TextNode,
+    extract_func: Callable[[str], List[tuple]],
+    format_func: Callable[[str, str], str],
+    new_type: TextType
+) -> List[TextNode]:
     tokens = extract_func(node.text)
-    if not tokens:
-        return [node]
-    
     token = tokens[0]
     delimiter = format_func(*token)
     head, sep, tail = node.text.partition(delimiter)
-    
+
     nodes: List[TextNode] = []
     if head:
         nodes.append(TextNode(head, TextType.NORMAL))
-    
+
     nodes.append(TextNode(token[0], new_type, token[1]))
-    
     if tail:
-        nodes.extend(_split_node(TextNode(tail, TextType.NORMAL), extract_func, format_func, new_type))
-    
+        nodes.append(TextNode(tail, TextType.NORMAL))
     return nodes
 
-def split_nodes(
-        nodes: List[TextNode],
-        extract_func: Callable[[str], List[tuple]],
-        format_func: Callable[[str, str], str],
-        new_type: TextType
-    ) -> List[TextNode]:
+def recursive_split_node(
+    node: TextNode,
+    extract_func: Callable[[str], List[tuple]],
+    format_func: Callable[[str, str], str],
+    new_type: TextType
+) -> List[TextNode]:
+    if not should_split(node, extract_func):
+        return [node]
+    
+    split_once = split_node_once(node, extract_func, format_func, new_type)
     result: List[TextNode] = []
+
+    for subnode in split_once:
+        if should_split(subnode, extract_func):
+            result.extend(recursive_split_node(subnode, extract_func, format_func, new_type))
+        else:
+            result.append(subnode)
+    return result
+
+
+def split_nodes(
+    nodes: List[TextNode],
+    extract_func: Callable[[str], List[tuple]],
+    format_func: Callable[[str, str], str],
+    new_type: TextType
+) -> List[TextNode]:
+    result: List[TextNode] = []
+
     for node in nodes:
-        result.extend(_split_node(node, extract_func, format_func, new_type))
+        result.extend(recursive_split_node(node, extract_func, format_func, new_type))
     return result
 
 def split_nodes_image(nodes: List[TextNode]) -> List[TextNode]:
